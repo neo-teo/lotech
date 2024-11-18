5fbbb0109c67c8e0d4bc65afa637b649c1bb125d
teo-clone <theodore.tsivranidis@gmail.com>
2024-11-18 00:32:06 -0500

    v0

---
+++ footer.js
@@ -0,0 +1,40 @@
+async function loadFooter() {
+    // Fetch the template
+    const response = await fetch('footer.html');
+    const text = await response.text();
+
+    // Create a temporary container and insert the template HTML
+    const container = document.createElement('div');
+    container.innerHTML = text;
+
+    // Get the template content
+    const template = container.querySelector('#footer-template');
+
+    // Clone and insert the template
+    const clone = template.content.cloneNode(true);
+    document.body.appendChild(clone);
+
+    // Initialize clock
+    const span = document.getElementById('clock');
+    function time() {
+        var options = {
+            timeZone: 'America/New_York',
+            hour12: false,
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        };
+        span.textContent = new Intl.DateTimeFormat('en-US', options)
+            .format(new Date());
+    }
+    time();
+    setInterval(time, 1000);
+
+    // Initialize weather
+    async function fetchWeather() {
+        const response = await fetch('https://wttr.in/New+York?format=%t');
+        const temperature = await response.text();
+        document.getElementById('weather').textContent = `${temperature}`;
+    }
+    fetchWeather();
+}
---
+++ works.js
@@ -0,0 +1,101 @@
+const works = [
+    {
+        title: "pygif",
+        image: "assets/images/works/bike.gif",
+        keywords: ["python", "pixel masking"],
+        link: "https://play.teo.fyi/pygif",
+    },
+    {
+        title: "impermanence",
+        image: "assets/images/works/impermanence.gif",
+        keywords: ["p5.js", "tool"],
+        link: "https://play.teo.fyi/impermanence",
+    },
+    {
+        title: "riso",
+        image: "assets/images/works/boys.gif",
+        keywords: ["p5.js", "generative"],
+        link: "https://play.teo.fyi/riso",
+    },
+    {
+        title: "sobu",
+        image: "assets/images/works/sobu.gif",
+        keywords: ["p5.js", "world", "figma / photoshop"],
+        link: "https://play.teo.fyi/sobu",
+    },
+    {
+        title: "liquids",
+        image: "assets/images/works/liquids.gif",
+        keywords: ["p5.js", "data driven"],
+        link: "https://play.teo.fyi/liquids",
+    },
+    {
+        title: "jellyfish",
+        image: "assets/images/works/jellies.gif",
+        keywords: ["p5.js", "world"],
+        link: "https://play.teo.fyi/jellyfish",
+    },
+    {
+        title: "cipher",
+        image: "assets/images/works/cipher.gif",
+        keywords: ["html / js / css", "tool"],
+        link: "https://play.teo.fyi/cipher",
+    },
+    {
+        title: "willump",
+        image: "assets/images/works/willump.gif",
+        keywords: ["p5.js", "world"],
+        link: "https://play.teo.fyi/willump",
+    },
+    {
+        title: "coins",
+        image: "assets/images/works/coins.gif",
+        keywords: ["p5.js", "data driven"],
+        link: "https://play.teo.fyi/coins",
+    },
+    {
+        title: "routine",
+        image: "assets/images/works/routine.gif",
+        keywords: ["html / js / css", "illustrator"],
+        link: "https://play.teo.fyi/routine",
+    },
+    {
+        title: "face",
+        image: "assets/images/works/face.gif",
+        keywords: ["p5.js", "world"],
+        link: "https://play.teo.fyi/face",
+    },
+    {
+        title: "ps2 nostalgia",
+        image: "assets/images/works/ps2.png",
+        keywords: ["svelte", "generative"],
+        link: "https://ps2nostalgia.win",
+    },
+];
+
+// Rendering function
+function renderWorks() {
+    const projectsList = document.getElementById('works');
+
+    works.forEach(project => {
+        const li = document.createElement('li');
+        li.className = 'work-item';
+
+        // Generate random rotation between -5 and 5 degrees
+        // const randomRotation = -Math.random() * 10 + Math.random() * 10;
+        const randomRotation = 0;
+
+        li.innerHTML = `
+        <a href="${project.link}">
+            <h3 style="transform: rotate(${randomRotation}deg)">${project.title}</h3>
+
+            <img src="${project.image}" alt="${project.title}" class="work-image">
+            <div class="keywords">
+                ${project.keywords.map(keyword => `<span class="keyword">${keyword}</span>`).join('')}
+            </div>
+        </a>
+        `;
+
+        projectsList.appendChild(li);
+    });
+} 
